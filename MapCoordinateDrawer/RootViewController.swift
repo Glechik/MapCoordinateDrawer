acaf7bcc28d108fc068e9299b07dbe7de18ebb03
Dmitry Savchenko <glechik11@mail.ru>
2016-05-13 04:00:27 +0300

    init

---
+++ MapCoordinateDrawer/RootViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  MapCoordinateDrawer
+//
+//  Created by Dmitry Savchenko on 13.05.16.
+//  Copyright © 2016 Dmitry Savchenko. All rights reserved.
+//
+
+import UIKit
+
+class RootViewController: UIViewController
+{
+
+}
+
